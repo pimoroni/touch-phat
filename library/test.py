f1a3806ea35c28b279be8a6b222f0600034dd0bb
Phil Howard <phil@gadgetoid.com>
2017-04-04 16:53:32 +0000

    Expanded test coverage

--- library/test.py
+++ library/test.py
@@ -1,17 +1,36 @@
+#!/usr/bin/env python
+
+import mock
+import sys
 import signal
 import time
 
+sys.modules['cap1xxx'] = mock.Mock()
+
 import touchphat
 
+def assert_raises(action, expect, message):
+    try:
+        action()
+    except expect:
+        return
+    except Exception as e:
+        print(message)
+        raise e
 
-for pad in ['Back','A','B','C','D','Enter']:
-    touchphat.set_led(pad, True)
-    time.sleep(0.5)
-    touchphat.set_led(pad, False)
-    time.sleep(0.5)
+def test_handler(evt):
+    pass
 
-@touchphat.on_touch(['Back','A','B','C','D','Enter'])
-def handle_touch(event):
-    print(event.name)
+assert hasattr(touchphat, "__version__") == True, "Touch pHAT module should include a __version__ attribute"
 
-signal.pause()
+print("Testing Touch pHAT version: {}".format(touchphat.__version__))
+
+assert touchphat.NAMES == ['Back', 'A', 'B', 'C', 'D', 'Enter'], "NAMES should be: 'Back', 'A', 'B', 'C', 'D', 'Enter'"
+
+assert touchphat.NUMMAP == [1, 2, 3, 4, 5, 6], "NUMMAP should be a list of numbers from 1 to 6"
+
+assert_raises(lambda: touchphat.set_led(0, True), ValueError, "touchpad.set_led(0, True) should raise ValueError")
+assert_raises(lambda: touchphat.on_touch(0, handler=test_handler), ValueError, "touchphat.on_touch(0, ...) should raise ValueError")
+assert_raises(lambda: touchphat.on_release(0, handler=test_handler), ValueError, "touchphat.on_release(0, ...) shoudl raise ValueError")
+
+print("Passed!")
