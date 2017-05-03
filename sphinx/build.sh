ca5c25ca8c2cbad44f0df4b97efe18a1cf9d4f4f
Phil Howard <phil@gadgetoid.com>
2017-05-03 16:24:44 +0100

    Removed cruft common to Sphinx build tools.

--- sphinx/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Per library build scripts have been deprecated."
-echo "Please clone github.com/pimoroni/tools"
-echo "and use the Sphinx build script there instead."
-echo ""
-echo "SRC_DIR=`pwd` ./build.sh"
